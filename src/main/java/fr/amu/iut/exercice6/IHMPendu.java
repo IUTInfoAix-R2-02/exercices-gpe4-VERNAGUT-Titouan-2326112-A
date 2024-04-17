8a66b95161cb67dabad4f9c71cbbc0455a744611
Vernagut <titouanvernagut@gmail.com>
2024-04-17 12:15:12 +0200

    exo 6 en cours

--- src/main/java/fr/amu/iut/exercice6/IHMPendu.java
+++ src/main/java/fr/amu/iut/exercice6/IHMPendu.java
@@ -1,11 +1,15 @@
 package fr.amu.iut.exercice6;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -23,38 +27,40 @@ public class IHMPendu extends Application {
         BorderPane root=new BorderPane();
         VBox lettre=new VBox();
 
-        TextField a=new TextField("A");
-        TextField e=new TextField("E");
-        TextField i=new TextField("I");
-        TextField o=new TextField("O");
-        TextField u=new TextField("U");
-        TextField y=new TextField("Y");
+        Button a=new Button("A");
+        Button e=new Button("E");
+        Button i=new Button("I");
+        Button o=new Button("O");
+        Button u=new Button("U");
+        Button y=new Button("Y");
         HBox ae=new HBox();
         ae.getChildren().addAll(a,e,i,o,u,y);
         ae.setAlignment(Pos.CENTER);
-        TextField b=new TextField("B");
-        TextField c=new TextField("C");
-        TextField d=new TextField("D");
-        TextField f=new TextField("F");
-        TextField g=new TextField("G");
-        TextField h=new TextField("H");
-        TextField j=new TextField("J");
-        TextField k=new TextField("K");
-        TextField l=new TextField("L");
-        TextField m=new TextField("M");
+
+        
+        Button b=new Button("B");
+        Button c=new Button("C");
+        Button d=new Button("D");
+        Button f=new Button("F");
+        Button g=new Button("G");
+        Button h=new Button("H");
+        Button j=new Button("J");
+        Button k=new Button("K");
+        Button l=new Button("L");
+        Button m=new Button("M");
         HBox bm=new HBox();
         bm.getChildren().addAll(b,c,d,f,g,h,j,k,l,m);
         bm.setAlignment(Pos.CENTER);
-        TextField n=new TextField("N");
-        TextField p=new TextField("P");
-        TextField q=new TextField("Q");
-        TextField r=new TextField("R");
-        TextField s=new TextField("S");
-        TextField t=new TextField("T");
-        TextField v=new TextField("V");
-        TextField w=new TextField("W");
-        TextField x=new TextField("X");
-        TextField z=new TextField("Z");
+        Button n=new Button("N");
+        Button p=new Button("P");
+        Button q=new Button("Q");
+        Button r=new Button("R");
+        Button s=new Button("S");
+        Button t=new Button("T");
+        Button v=new Button("V");
+        Button w=new Button("W");
+        Button x=new Button("X");
+        Button z=new Button("Z");
         root.setBottom(lettre);
         HBox boutonbas=new HBox();
         Button rejouer=new Button("Rejoue Pélo");
@@ -63,23 +69,114 @@ public class IHMPendu extends Application {
         HBox nz=new HBox();
         nz.getChildren().addAll(n,p,q,r,s,t,v,w,x,z);
         nz.setAlignment(Pos.CENTER);
+        TextField barre=new TextField();
+        barre.setAlignment(Pos.CENTER);
         lettre.getChildren().addAll(
+                barre,
                 ae,
                 bm,
                 nz,
                 boutonbas
         );
         root.setBottom(lettre);
-        HBox mot=new HBox();
-        Dico newDick=new Dico();
-        String motCache = new String();
-        motCache=newDick.getMot();
-        for(int I=0 ; I<motCache.length();I++){
-            motCache=motCache+"*";
+        VBox mot = new VBox();
+        HBox vies=new HBox();
+
+        int vie = 7;
+        Label nbrevie=new Label("nombre de vie: "+ vie);
+
+        Dico dico = new Dico();
+        String motCache = dico.getMot();
+        String motCacheAffiche = "";
+        for (int I = 0; I < motCache.length(); I++) {
+            motCacheAffiche += "*";
         }
-        Label etiquette=new Label(motCache);
-        mot.getChildren().addAll(etiquette);
-        root.setCenter(etiquette);
+        Label etiquette = new Label(motCacheAffiche);
+        etiquette.setStyle("-fx-font-size: 20px;");
+        mot.getChildren().addAll(nbrevie,etiquette);
+        mot.setAlignment(Pos.CENTER);
+        root.setCenter(mot);
+        a.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'z');
+        });
+        z.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'z');
+        });
+        e.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'e');
+        });
+        r.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'r');
+        });
+        t.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 't');
+        });
+        y.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'y');
+        });
+        u.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'u');
+        });
+        i.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'i');
+        });
+        o.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'o');
+        });
+        p.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'p');
+        });
+        q.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'q');
+        });
+        s.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 's');
+        });
+        d.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'd');
+        });
+        f.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'f');
+        });
+        g.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'g');
+        });
+        h.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'h');
+        });
+        j.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'j');
+        });
+        k.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'k');
+        });
+        l.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'l');
+        });
+        m.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'm');
+        });
+        w.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'w');
+        });
+        x.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'x');
+        });
+        c.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'c');
+        });
+        v.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'v');
+        });
+        b.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'b');
+        });
+        n.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            barre.setText(barre.getText() + 'n');
+        });
+
+
+
 
         primaryStage.setTitle("Jeu du pendu");
 
@@ -91,6 +188,7 @@ public class IHMPendu extends Application {
         primaryStage.setScene( scene );
         primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
